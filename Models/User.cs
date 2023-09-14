e5d6d5b9082c30b24698bb6420af228e9bf70db6
sunder2568223 <sunderyssr@gmail.com>
2023-09-14 10:59:23 +0530

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppUsers.Models
+{
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+
+    }
+}
